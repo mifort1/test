90c1d4df14bbdfece5a085d51e3ec61e930a8e33
mifort1 <mifort@ukr.net>
2015-06-26 18:48:27 +0300

    удалил www

--- www/dert.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mifort
- * Date: 26.06.2015
- * Time: 16:02
- */
-//
--- www/function.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mifort
- * Date: 26.06.2015
- * Time: 15:39
- */
-// привет
