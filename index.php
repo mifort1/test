0fc2e886bfad2914a5ccc5330c4417e0b6377726
mifort1 <mifort@ukr.net>
2016-05-11 15:00:43 +0300

    + mod_rewrite

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
  * Time: 11:05
  */
 //error_reporting(E_ALL & ~E_NOTICE);
+
 require_once __DIR__.'/autoload.php';
+
+
+$url = $_SERVER['REQUEST_URI'];
 
 $controller = new \App\Controllers\News();
 $action = !empty($_GET['action']) ? $_GET['action'] : 'Index';
